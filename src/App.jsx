becc9529171799e7744229fc2748ba4c184bbcaf
Mostafa Nasser <mostafanasser23456789@gmail.com>
2025-01-07 17:21:40 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Home from "./Components/Home/Home";
+import NotFound from './Components/Notfound/Notfound';
 const routes = createBrowserRouter([
   {
     path: '/', 
@@ -15,6 +16,7 @@ const routes = createBrowserRouter([
       { path: 'about', element: <About /> },
       { path: 'portfolio', element: <Portfolio /> },
       { path: 'contact', element: <Contact /> },
+      { path: '*', element: <NotFound /> } 
     ]
   }
 ]);
